252103670d8de40e6d9073f6785e11c3f8c28202
Adam Poloczek <piratjsk@gmail.com>
2018-08-23 16:41:18 +0200

    add empty egg recipe

--- src/main/java/net/piratjsk/eggit/EggIt.java
+++ src/main/java/net/piratjsk/eggit/EggIt.java
@@ -1,13 +1,29 @@
 package net.piratjsk.eggit;
 
 import net.piratjsk.eggit.listeners.CatchMobListener;
+import org.bukkit.ChatColor;
+import org.bukkit.Material;
+import org.bukkit.NamespacedKey;
+import org.bukkit.inventory.ItemStack;
+import org.bukkit.inventory.ShapedRecipe;
+import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.java.JavaPlugin;
 
 public final class EggIt extends JavaPlugin {
 
     @Override
     public void onEnable() {
+        this.saveDefaultConfig();
         this.getServer().getPluginManager().registerEvents(new CatchMobListener(this), this);
+        final ItemStack egg = new ItemStack(Material.EGG);
+        final ItemMeta meta = egg.getItemMeta();
+        meta.setDisplayName(ChatColor.RESET + "Empty Egg");
+        egg.setItemMeta(meta);
+        final ShapedRecipe recipe = new ShapedRecipe(NamespacedKey.minecraft(this.getName().toLowerCase()), egg);
+        recipe.shape(" I ", "IDI", " I ");
+        recipe.setIngredient('I', Material.IRON_INGOT);
+        recipe.setIngredient('D', Material.DIAMOND);
+        this.getServer().addRecipe(recipe);
     }
 
 }
